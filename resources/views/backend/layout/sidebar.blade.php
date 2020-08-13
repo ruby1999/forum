e5efdd656c54a51805739c11c0af68519b742869
ruby_1999 <ruby.1999.0220@gmail.com>
2020-08-13 13:40:05 +0800

    basic sidebar(但是下拉會跳)

--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -1,44 +1,5 @@
 <nav class="sidebar sidebar-offcanvas dynamic-active-class-disabled" id="sidebar">
   <ul class="nav">
-    <li class="nav-item nav-profile not-navigation-link">
-      <div class="nav-link">
-        <div class="user-wrapper">
-          <div class="profile-image">
-            <img src="{{ url('assets/images/faces/face8.jpg') }}" alt="profile image">
-          </div>
-          <div class="text-wrapper">
-            <p class="profile-name">Richard V.Welsh</p>
-            <div class="dropdown" data-display="static">
-              <a href="#" class="nav-link d-flex user-switch-dropdown-toggler" id="UsersettingsDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                <small class="designation text-muted">Manager</small>
-                <span class="status-indicator online"></span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="UsersettingsDropdown">
-                <a class="dropdown-item p-0">
-                  <div class="d-flex border-bottom">
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                      <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                    </div>
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                      <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                    </div>
-                    <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                      <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                    </div>
-                  </div>
-                </a>
-                <a class="dropdown-item mt-2"> Manage Accounts </a>
-                <a class="dropdown-item"> Change Password </a>
-                <a class="dropdown-item"> Check Inbox </a>
-                <a class="dropdown-item"> Sign Out </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button class="btn btn-success btn-block">New Project <i class="mdi mdi-plus"></i>
-        </button>
-      </div>
-    </li>
     <li class="nav-item {{ active_class(['/']) }}">
       <a class="nav-link" href="{{ url('/') }}">
         <i class="menu-icon mdi mdi-television"></i>
@@ -46,12 +7,12 @@
       </a>
     </li>
     <li class="nav-item {{ active_class(['basic-ui/*']) }}">
-      <a class="nav-link" data-toggle="collapse" href="#basic-ui" aria-expanded="{{ is_active_route(['basic-ui/*']) }}" aria-controls="basic-ui">
+      <a class="nav-link" data-toggle="collapse" href="#basic-ui" aria-expanded="" aria-controls="basic-ui">
         <i class="menu-icon mdi mdi-dna"></i>
         <span class="menu-title">Basic UI Elements</span>
         <i class="menu-arrow"></i>
       </a>
-      <div class="collapse {{ show_class(['basic-ui/*']) }}" id="basic-ui">
+      <div class="collapse {{ active_class(['basic-ui/*']) }}" id="basic-ui">
         <ul class="nav flex-column sub-menu">
           <li class="nav-item {{ active_class(['basic-ui/buttons']) }}">
             <a class="nav-link" href="{{ url('/basic-ui/buttons') }}">Buttons</a>
@@ -85,12 +46,12 @@
       </a>
     </li>
     <li class="nav-item {{ active_class(['user-pages/*']) }}">
-      <a class="nav-link" data-toggle="collapse" href="#user-pages" aria-expanded="{{ is_active_route(['user-pages/*']) }}" aria-controls="user-pages">
+      <a class="nav-link" data-toggle="collapse" href="#user-pages" aria-expanded="" aria-controls="user-pages">
         <i class="menu-icon mdi mdi-lock-outline"></i>
         <span class="menu-title">User Pages</span>
         <i class="menu-arrow"></i>
       </a>
-      <div class="collapse {{ show_class(['user-pages/*']) }}" id="user-pages">
+      <div class="collapse {{ active_class(['user-pages/*']) }}" id="user-pages">
         <ul class="nav flex-column sub-menu">
           <li class="nav-item {{ active_class(['user-pages/login']) }}">
             <a class="nav-link" href="{{ url('/user-pages/login') }}">Login</a>
